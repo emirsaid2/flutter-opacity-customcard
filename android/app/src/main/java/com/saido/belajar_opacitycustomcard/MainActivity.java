4fb2a3c96ac21aaa3f5f7d3f65f50d6f9920cbcf
saidemirfaisal <emirsaid2@gmail.com>
2019-08-02 22:06:57 +0700

    first commit

---
+++ android/app/src/main/java/com/saido/belajar_opacitycustomcard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.saido.belajar_opacitycustomcard;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
